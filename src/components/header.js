30219061377232ebfa7673b0cd14bed034635aad
Joshua Lee <jminlee24+1@outlook.com>
2022-10-01 10:39:50 -0400

    init commit

---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="heading">
+      <h1 className="headingTitle">ZaJeJoDarward</h1>
+    </div>
+  );
+};
+
+export default Header;
