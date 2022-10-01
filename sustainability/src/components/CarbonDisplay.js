ac993550f5db385f45aa7f805f61c49a92366e62
Joshua Lee <jminlee24+1@outlook.com>
2022-10-01 13:59:47 -0400

    carbon display stuff

--- sustainability/src/components/CarbonDisplay.js
+++ sustainability/src/components/CarbonDisplay.js
@@ -4,7 +4,10 @@ import "./Home.css";
 const CarbonDisplay = () => {
   return (
     <div className="carbonDisplay">
-      <h2>Carbon Emissions</h2>
+      <h4 style={{ flex: 1 }}>Carbon Emissions</h4>
+      <div className="carbonDisplayInfo" style={{ flex: 9 }}>
+        DISPLAY CONTENT INFO
+      </div>
     </div>
   );
 };
